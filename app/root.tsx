1b02ad6825028b962759da97d82f56a3a04c4dba
K Kiasat <kk107@hw.ac.uk>
2024-03-25 06:29:11 +0400

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -8,7 +8,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-import stylesheet from "~/styles/tailwind.css";
+import stylesheet from "./styles/tailwind.css";
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : [{ rel: "stylesheet", href: stylesheet }]),
